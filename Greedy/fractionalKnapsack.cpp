0899e0510f86982d8be1b4e74eabcf34758b261f
Amol39 <amolchavan474004@gmail.com>
2024-02-14 21:43:05 +0530

    .

---
+++ Greedy/fractionalKnapsack.cpp
@@ -0,0 +1,17 @@
+/*
+Given the weights and profits of N items, in the form of {profit,weight} put these items in a knapsack of capacity W to get the maximum total profit in the knapsack. In fractional knapsack, we can break items for maximizing the total value of the knapsack.
+
+input  arr[]= {{60,10},{100,20},{120,30}}, W = 50
+output : 240
+
+Explanation : By taking items of weight 10 and 20 kg and 2/3 of 30 kg.
+Hence total price will be 60+100+(2/3)(120) = 240
+*/
+
+#include<iostream>
+using namespace std ;
+
+int main(){
+    
+    return 0;
+}
