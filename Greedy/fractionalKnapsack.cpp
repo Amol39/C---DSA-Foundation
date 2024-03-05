3c3ec3ec24973572776e93aafc4bda429e8e424a
Amol39 <amolchavan474004@gmail.com>
2024-03-05 16:22:14 +0530

    .

--- Greedy/fractionalKnapsack.cpp
+++ Greedy/fractionalKnapsack.cpp
@@ -8,50 +8,59 @@ Explanation : By taking items of weight 10 and 20 kg and 2/3 of 30 kg.
 Hence total price will be 60+100+(2/3)(120) = 240
 */
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std ;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-struct Item{
-    int  value;
+class Item {
+public:
+    int value;
     int weight;
+
+    Item(int v, int w) {
+        value = v;
+        weight = w
+    } 
 };
 
-bool cmp(Item i1, Item i2){
-    double v_w_i1 = static_cast<double>(i1.value)/i1.weight;
-    double v_w_i2 = static_cast<double>(i2.value)/i2.weight;
-    return v_w_i1>v_w_i2;
+bool cmp(const Item &i1, const Item &i2) {
+    double v_w_i1 = static_cast<double>(i1.value) / i1.weight;
+    double v_w_i2 = static_cast<double>(i2.value) / i2.weight;
+    return v_w_i1 > v_w_i2;
 }
 
-double  fractional(int W, vector<Item>& items){
-    double ans = 0;
-    sort(items.begin(),items.end(),cmp);
-    for(const auto item:items){
-        if(item.weight <=W){
-            ans += item.value;
-            W -= item.weight;
-        }else{
-            double fraction = static_cast<double>(W)/item.weight;
-            ans += fraction * item.value;
-            W = 0;
+class FractionalKnapsack {
+public:
+    double solve(int W, vector<Item> &items) {
+        double ans = 0;
+        sort(items.begin(), items.end(), cmp);
+        for (const auto &item : items) {
+            if (item.weight <= W) {
+                ans += item.value;
+                W -= item.weight;
+            } else {
+                double fraction = static_cast<double>(W) / item.weight;
+                ans += fraction * item.value;
+                W = 0;
+            }
         }
+        return ans;
     }
-    return ans;
-}
+};
 
-int main(){
+int main() {
     int n, W;
-    cin>> n >> W;
+    cin >> n >> W;
     vector<Item> items;
-    for(int i=0;i<n;i++){
-        int v,w;
-        cin>>v>>w;
-        Item it;
-        it.value = v;
-        it.weight = w;
+    for (int i = 0; i < n; i++) {
+        int v, w;
+        cin >> v >> w;
+        Item it(v, w);
         items.push_back(it);
     }
-    cout<<fractional(W, items)<<"\n";
+
+    FractionalKnapsack knapsack;
+    cout << knapsack.solve(W, items) << "\n";
     return 0;
 }
