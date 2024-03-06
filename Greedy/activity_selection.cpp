0dc1eceea68d1376a2d124ba1454ed0743b9e046
Amol39 <amolchavan474004@gmail.com>
2024-03-06 21:42:16 +0530

    .

---
+++ Greedy/activity_selection.cpp
@@ -0,0 +1,29 @@
+/*
+Given n activities with their start and finish day given in array start[] and end[]. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a given day. 
+Note : Duration of the activity  includes both starting and ending day.
+
+input : N = 4
+start[] = {1,3,2,5}
+end[] = {2,4,3,6}
+
+output = 3
+
+explanation :- A person can perform activities 1,2 and 4
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct activity{
+    int start;
+    int end;
+};
+
+
+
+int main(){
+    return 0;
+}
